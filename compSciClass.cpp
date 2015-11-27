5fee9ab750ca136cf907a3d1c32f781936cc39f3
JonOrnBrynjarsson <jonoganna@hotmail.com>
2015-11-27 14:24:35 +0000

    Fyllti upp í klalsann
    Fyllti inn í compsciclass klasann og commentaði h skránna.

--- compSciClass.cpp
+++ compSciClass.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-compSciClass::compsciclass()
+compsciclass::compsciclass()
 {
     name = '';
     gender = '';
@@ -13,4 +13,71 @@ compSciClass::compsciclass()
     yearOfDeath=0;
     description = '';
     link = '';
+}
+compsciclass::compsciclass(string name, bool sex, int yoBirth,int yoDeath, string desc, string link)
+{
+    name = name;
+    gender = sex;
+    yearOfBirth = yoBirth;
+    yearOfDeath = yoDeath;
+    description = desc;
+    link = link;
+}
+
+
+
+string compsciclass::getName() const
+{
+    return name;
+}
+
+bool compsciclass::getgender() const
+{
+    return gender;
+}
+int compsciclass::getYearOfBirth() const
+{
+    return yearOfBirth;
+}
+int compsciclass::getYearOfDeath() const
+{
+    return yearOfDeath;
+}
+string compsciclass::getDescription() const
+{
+    return description;
+}
+string compsciclass::getLink() const
+{
+    return link;
+}
+
+void compsciclass::setName(string name)
+{
+    name = name;
+}
+
+void compsciclass::setgender(bool sex)
+{
+    gender = sex;
+}
+
+void compsciclass::setYearOfBirth(int birthYear)
+{
+    yearOfBirth = birthYear;
+}
+
+void compsciclass::setYearOfDeath(int deathYear)
+{
+    yearOfDeath = deathYear;
+}
+
+void compsciclass::setDescription(string desc)
+{
+    description = desc;
+}
+
+void compsciclass::setLink(string url)
+{
+    link = url;
 }
