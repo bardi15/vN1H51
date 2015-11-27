0e84a621e381d1d6e65e940f8d4713e809059f30
bardi15 <bardi15@ru.is>
2015-11-27 13:50:10 +0000

    setti inn útfærslu á infodisplay, sem er presentation klassinn okkar

--- infodisplay.cpp
+++ infodisplay.cpp
@@ -1,7 +1,46 @@
 #include "infodisplay.h"
+#include "compsciclass.h"
+#include "workingclass.h"
+#include <iostream>
+
+using namespace std;
 
 infoDisplay::infoDisplay()
 {
 
 }
 
+void infoDisplay::splashScreen()
+{
+
+}
+
+void infoDisplay::screenSelection()
+{
+
+}
+
+void infoDisplay::displayAddScientist()
+{
+
+}
+
+void infoDisplay::displayRemoveScientist()
+{
+
+}
+
+void infoDisplay::displayChangeScientist()
+{
+
+}
+
+void infoDisplay::displaySearchScientist()
+{
+
+}
+
+void infoDisplay::quitProgram()
+{
+
+}
--- infodisplay.h
+++ infodisplay.h
@@ -1,11 +1,20 @@
 #ifndef INFODISPLAY_H
 #define INFODISPLAY_H
 #include "compsciclass.h"
+#include "infodisplay.h"
+#include "workingclass.h"
 
 class infoDisplay
 {
 public:
     infoDisplay();
+    void splashScreen();
+    void screenSelection();
+    void displayAddScientist();
+    void displayRemoveScientist();
+    void displayChangeScientist();
+    void displaySearchScientist();
+    void quitProgram();
 };
 
 #endif // INFODISPLAY_H
