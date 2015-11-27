538e9bc1200939cfbdbf10b59ee40f276b797e02
JonOrnBrynjarsson <jonoganna@hotmail.com>
2015-11-27 16:39:11 +0000

    WorkingClass Vector
    Fékk inputið í vector til að virka með því að búa senda tilvik af
    klasanum inn í workingclass og vera með privat breytu þar sem er vector.

--- workingclass.cpp
+++ workingclass.cpp
@@ -5,3 +5,7 @@ WorkingClass::WorkingClass()
 
 }
 
+WorkingClass::tovector(compsciclass input_dot)
+{
+    push_back(input_dot);
+}
--- workingclass.h
+++ workingclass.h
@@ -1,13 +1,16 @@
 #ifndef WORKINGCLASS_H
 #define WORKINGCLASS_H
 #include "compsciclass.h"
+#include <vector>
 
 class WorkingClass
 {
 public:
     WorkingClass();
+    tovector(compsciclass input_dot, vector<compsciclass> invect);
+
 private:
-    
+    vector workingVector;
 };
 
 #endif // WORKINGCLASS_H
