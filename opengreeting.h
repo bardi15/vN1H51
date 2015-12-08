72ae4e5ff741aaf094f0e603ab5bd3a6de20581f
bardi15 <bardi15@ru.is>
2015-12-08 21:16:45 +0000

    kommentaði opengreeting

--- opengreeting.h
+++ opengreeting.h
@@ -1,11 +1,7 @@
 #ifndef OPENGREETING_H
 #define OPENGREETING_H
 
-//#include "infodisplay.h"
-
-#include <math.h>
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
@@ -13,10 +9,20 @@ class opengreeting
 {
 public:
     opengreeting();
+    //Default constructor for class.
+
     void printscreen (char ascii [][80]);
+    //  Precondition:   array has been filled.
+    //  Postcondition:  array is printed to screen.
     void fillarray (char ascii [][80], char input);
+    //  Precondition:   array is empty.
+    //  Postcondition:  array has been filled with spaces.
     void art (char ascii [][80]);
+    //  Precondition:   array is filled with spaces
+    //  Postcondition:  X is added to the array in specific areas.
     void greetingPost();
+    //  Precondition:   screen is blank
+    //  Postcondition:  ascii art has been printed to screen.
 
     char input;
     char ascii [20][80];
